140d931a8de3d76bb224b7fe58dd1b44bc533649
Pavol Juhas <pavol.juhas@gmail.com>
2008-12-10 04:36:46 +0000

    alpha0 release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import fix_setuptools_chmod
 # define distribution
 dist = setup(
         name = "diffpy.srreal",
-        version = "0.1a",
+        version = "0.1a0",
         namespace_packages = ['diffpy'],
         packages = find_packages(exclude=['PDFAPI']),
         entry_points = {
