bd96e119292c717939dc3e1923fd2bf04946e4b5
Christopher Farrow <farrowch@gmail.com>
2009-01-20 14:22:51 +0000

    Fixed import statement

--- diffpy/srreal/pdf/__init__.py
+++ diffpy/srreal/pdf/__init__.py
@@ -3,6 +3,6 @@ __id__ = "$Id$"
 
 from pdfmodel import PDFModel
 from phases import CrystalPhase
-from Lattice import Lattice
+from lattice import Lattice
 from atoms import Atom
 from converters import phaseFromStructure, structureFromPhase
