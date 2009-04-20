58cffb9a3a380e9cfaac5e706f4b0bf72eb4388d
Pavol Juhas <pavol.juhas@gmail.com>
2009-04-20 01:05:03 +0000

    Added standard comment header, no change in the code.

--- libsrreal/diffpy/mathutils.hpp
+++ libsrreal/diffpy/mathutils.hpp
@@ -1,3 +1,23 @@
+/*****************************************************************************
+*
+* diffpy.srreal     by DANSE Diffraction group
+*                   Simon J. L. Billinge
+*                   (c) 2009 Trustees of the Columbia University
+*                   in the City of New York.  All rights reserved.
+*
+* File coded by:    Pavol Juhas
+*
+* See AUTHORS.txt for a list of people who contributed.
+* See LICENSE.txt for license information.
+*
+******************************************************************************
+*
+* Various common mathematical constants and functions.
+*
+* $Id$
+*
+*****************************************************************************/
+
 #ifndef MATHUTILS_HPP_INCLUDED
 #define MATHUTILS_HPP_INCLUDED
 
