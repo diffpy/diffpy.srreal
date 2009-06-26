b06e3e0aa20db83207b259124241144f16638949
Pavol Juhas <pavol.juhas@gmail.com>
2009-06-26 01:41:18 +0000

    class GaussPeakProfile separated to its own files.

---
+++ libsrreal/diffpy/srreal/GaussPeakProfile.cpp
@@ -0,0 +1,93 @@
+/*****************************************************************************
+*
+* diffpy.srreal     by DANSE Diffraction group
+*                   Simon J. L. Billinge
+*                   (c) 2009 Trustees of the Columbia University
+*                   in the City of New York.  All rights reserved.
+*
+* File coded by:    Pavol Juhas
+*
+* See AUTHORS.txt for a list of people who contributed.
+* See LICENSE.txt for license information.
+*
+******************************************************************************
+*
+* class GaussPeakProfile -- full scale concrete implementation of the
+*     PeakProfile class.  GaussPeakProfile is registered as "gauss".
+*
+* $Id$
+*
+*****************************************************************************/
+
+#include <cmath>
+
+#include <diffpy/srreal/GaussPeakProfile.hpp>
+#include <diffpy/mathutils.hpp>
+
+using namespace std;
+
+namespace diffpy {
+namespace srreal {
+
+// Constructors --------------------------------------------------------------
+
+PeakProfile* GaussPeakProfile::create() const
+{
+    PeakProfile* rv = new GaussPeakProfile();
+    return rv;
+}
+
+
+PeakProfile* GaussPeakProfile::copy() const
+{
+    PeakProfile* rv = new GaussPeakProfile(*this);
+    return rv;
+}
+
+// Public Methods ------------------------------------------------------------
+
+const string& GaussPeakProfile::type() const
+{
+    static string rv = "gauss";
+    return rv;
+}
+
+
+double GaussPeakProfile::y(double x, double fwhm) const
+{
+    double xrel = x / fwhm;
+    double rv = 2 * sqrt(M_LN2 / M_PI) / fwhm * exp(-4 * M_LN2 * xrel * xrel);
+    return rv;
+}
+
+
+double GaussPeakProfile::xboundlo(double fwhm) const
+{
+    return -1 * this->xboundhi(fwhm);
+}
+
+
+double GaussPeakProfile::xboundhi(double fwhm) const
+{
+    double rv = (fwhm <= 0.0) ? 0.0 : (mhalfboundrel * fwhm);
+    return rv;
+}
+
+
+void GaussPeakProfile::setPrecision(double eps)
+{
+    using diffpy::mathutils::DOUBLE_MAX;
+    this->PeakProfile::setPrecision(eps);
+    if (eps <= 0.0)  mhalfboundrel = DOUBLE_MAX;
+    else if (eps < 1.0)  mhalfboundrel = sqrt(-log(eps) / (4 * M_LN2));
+    else  mhalfboundrel = 0.0;
+}
+
+// Registration --------------------------------------------------------------
+
+bool reg_GaussPeakProfile = registerPeakProfile(GaussPeakProfile());
+
+}   // namespace srreal
+}   // namespace diffpy
+
+// End of file
---
+++ libsrreal/diffpy/srreal/GaussPeakProfile.hpp
@@ -0,0 +1,56 @@
+/*****************************************************************************
+*
+* diffpy.srreal     by DANSE Diffraction group
+*                   Simon J. L. Billinge
+*                   (c) 2009 Trustees of the Columbia University
+*                   in the City of New York.  All rights reserved.
+*
+* File coded by:    Pavol Juhas
+*
+* See AUTHORS.txt for a list of people who contributed.
+* See LICENSE.txt for license information.
+*
+******************************************************************************
+*
+* class GaussPeakProfile -- full scale concrete implementation of the
+*     PeakProfile class.  GaussPeakProfile is registered as "gauss".
+*
+* $Id$
+*
+*****************************************************************************/
+
+#ifndef GAUSSPEAKPROFILE_HPP_INCLUDED
+#define GAUSSPEAKPROFILE_HPP_INCLUDED
+
+#include <diffpy/srreal/PeakProfile.hpp>
+
+namespace diffpy {
+namespace srreal {
+
+class GaussPeakProfile : public PeakProfile
+{
+    public:
+
+        // constructors
+        PeakProfile* create() const;
+        PeakProfile* copy() const;
+        GaussPeakProfile() : mhalfboundrel(0.0)  { }
+
+        // methods
+        const std::string& type() const;
+        double y(double x, double fwhm) const;
+        double xboundlo(double fwhm) const;
+        double xboundhi(double fwhm) const;
+        void setPrecision(double eps);
+
+    private:
+
+        // data
+        double mhalfboundrel;
+
+};
+
+}   // namespace srreal
+}   // namespace diffpy
+
+#endif  // GAUSSPEAKPROFILE_HPP_INCLUDED
