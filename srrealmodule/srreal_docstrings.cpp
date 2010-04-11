8ed4a50421fef924cbf72bbc15a17b3be9e1a052
Pavol Juhas <pavol.juhas@gmail.com>
2010-04-11 20:22:37 +0000

    Forgot to add these.

---
+++ srrealmodule/srreal_docstrings.cpp
@@ -0,0 +1,40 @@
+/*****************************************************************************
+*
+* diffpy.srreal     by DANSE Diffraction group
+*                   Simon J. L. Billinge
+*                   (c) 2010 Trustees of the Columbia University
+*                   in the City of New York.  All rights reserved.
+*
+* File coded by:    Pavol Juhas
+*
+* See AUTHORS.txt for a list of people who contributed.
+* See LICENSE.txt for license information.
+*
+******************************************************************************
+*
+* Python docstrings for the wrapped functions and classes.
+*
+* $Id$
+*
+*****************************************************************************/
+
+namespace srrealmodule {
+
+// BasePairQuantity ----------------------------------------------------------
+
+const char* doc_BasePairQuantity_value = "\
+Return a copy of the internal total contributions.\n\
+";
+
+const char* doc_BasePairQuantity_eval = "\
+Calculate a pair quantity for the specified structure.\n\
+\n\
+stru -- structure object that can be converted to StructureAdapter\n\
+\n\
+Return a copy of the internal total contributions.\n\
+May need to be further transformed to get the desired value.\n\
+";
+
+}   // namespace srrealmodule
+
+// End of file
---
+++ srrealmodule/srreal_docstrings.hpp
@@ -0,0 +1,31 @@
+/*****************************************************************************
+*
+* diffpy.srreal     by DANSE Diffraction group
+*                   Simon J. L. Billinge
+*                   (c) 2009 Trustees of the Columbia University
+*                   in the City of New York.  All rights reserved.
+*
+* File coded by:    Pavol Juhas
+*
+* See AUTHORS.txt for a list of people who contributed.
+* See LICENSE.txt for license information.
+*
+******************************************************************************
+*
+* Python docstrings for the wrapped functions and classes.
+*
+* $Id$
+*
+*****************************************************************************/
+
+#ifndef SRREAL_DOCSTRINGS_HPP_INCLUDED
+#define SRREAL_DOCSTRINGS_HPP_INCLUDED
+
+namespace srrealmodule {
+
+extern const char* doc_BasePairQuantity_value;
+extern const char* doc_BasePairQuantity_eval;
+
+}   // namespace srrealmodule
+
+#endif  // SRREAL_DOCSTRINGS_HPP_INCLUDED
