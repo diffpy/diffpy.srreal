64c66d2f2cfad5a52826f45744ab4de0583d3f5e
Pavol Juhas <pavol.juhas@gmail.com>
2010-04-14 06:45:52 +0000

    The base Attributes class now has `__getattr__` and `__setattr__` forwarding to _getDoubleAttr and _setDoubleAttr C++ methods. Sweet.

--- srrealmodule/wrap_Attributes.cpp
+++ srrealmodule/wrap_Attributes.cpp
@@ -30,6 +30,25 @@ namespace nswrap_Attributes {
 DECLARE_PYSET_METHOD_WRAPPER(namesOfDoubleAttributes,
         namesOfDoubleAttributes_asset)
 
+
+const char* getattr_setattr_code = "\
+def __getattr__(self, name):\n\
+    try:\n\
+        rv = self._getDoubleAttr(name)\n\
+    except Exception, e:\n\
+        raise AttributeError(e)\n\
+    return rv\n\
+\n\
+\n\
+def __setattr__(self, name, value):\n\
+    if self._hasDoubleAttr(name):\n\
+        self._setDoubleAttr(name, value)\n\
+    else:\n\
+        object.__setattr__(self, name, value)\n\
+    return\n\
+";
+
+
 }   // namespace nswrap_Attributes
 
 // Wrapper definition --------------------------------------------------------
@@ -39,15 +58,20 @@ void wrap_Attributes()
     using diffpy::Attributes;
     using namespace boost::python;
     using namespace nswrap_Attributes;
-
+    // store custom __getattr__ and __setattr__ in the locals dictionary
+    object globals = import("__main__").attr("__dict__");
+    dict locals;
+    exec(getattr_setattr_code, globals, locals);
+    // ready for class definition
     class_<Attributes>("Attributes")
         .def("_getDoubleAttr", &Attributes::getDoubleAttr)
         .def("_setDoubleAttr", &Attributes::setDoubleAttr)
         .def("_hasDoubleAttr", &Attributes::hasDoubleAttr)
         .def("_namesOfDoubleAttributes",
                 namesOfDoubleAttributes_asset<Attributes>)
+        .def("__getattr__", locals["__getattr__"])
+        .def("__setattr__", locals["__setattr__"])
         ;
-
 }
 
 }   // namespace srrealmodule
