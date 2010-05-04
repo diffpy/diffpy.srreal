8f3afc133d00ec8d2bc0336073d76bccace0652e
Pavol Juhas <pavol.juhas@gmail.com>
2010-05-04 20:27:55 +0000

    Updated for interface changes to DebyePDFCalculator.

--- examples/compareC60PDFs.py
+++ examples/compareC60PDFs.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
 
 """Plot C60 PDFs calculated with PDFCalculator and DebyePDFCalculator.
+The C60 molecule is held in a diffpy.Structure object.
 """
 
+import sys
 from pylab import plot, show, clf, draw
-
 from diffpy.Structure import Structure
-from diffpy.srreal.pdfcalculator import PDFCalculator
-from diffpy.srreal.srreal_ext import DebyePDFCalculator_ext
+from diffpy.srreal.pdfcalculator import PDFCalculator, DebyePDFCalculator
 
-bucky = Structure(filename='C60bucky.stru')
+# load C60 molecule as a diffpy.Structure object
+bucky = Structure(filename='datafiles/C60bucky.stru')
 cfg = { 'qmax' : 25,
         'rmin' : 0,
         'rmax' : 10.001,
         'rstep' : 0.05,
 }
 
+# calculate PDF by real-space summation
 pc0 = PDFCalculator(**cfg)
 r0, g0 = pc0(bucky)
 
-pc1 = DebyePDFCalculator_ext()
-for k, v in cfg.items():
-    pc1._setDoubleAttr(k, v)
-pc1.eval(bucky)
-r1 = pc1.getRgrid()
-g1 = pc1.getPDF()
+# calculate PDF using Debye formula
+pc1 = DebyePDFCalculator(**cfg)
+r1, g1 = pc1(bucky)
 gd = g0 - g1
 
+# plot both results and the difference curve
 clf()
 plot(r0, g0, r1, g1, r0, gd - 1)
 show()
