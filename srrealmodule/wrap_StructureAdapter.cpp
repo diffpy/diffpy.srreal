bc88a1861794a7f9999ad0acf10261096c64aa22
Pavol Juhas <pavol.juhas@gmail.com>
2010-05-24 17:32:07 +0000

    Improved StructureAdapter wrapper.  Pure virtual methods marked as such. Added proper wrappers to virtual methods with default implementations.

--- srrealmodule/wrap_StructureAdapter.cpp
+++ srrealmodule/wrap_StructureAdapter.cpp
@@ -61,6 +61,51 @@ class StructureAdapterWrap :
         }
 
 
+        double totalOccupancy() const
+        {
+            override f = this->get_override("totalOccupancy");
+            if (f)  return f();
+            return this->default_totalOccupancy();
+        }
+
+        double default_totalOccupancy() const
+        {
+            return this->StructureAdapter::totalOccupancy();
+        }
+
+
+        double numberDensity() const
+        {
+            override f = this->get_override("numberDensity");
+            if (f)  return f();
+            return this->default_numberDensity();
+        }
+
+        double default_numberDensity() const
+        {
+            return this->StructureAdapter::numberDensity();
+        }
+
+
+        const std::string& siteAtomType(int idx) const
+        {
+            static std::string rv;
+            override f = this->get_override("siteAtomType");
+            if (f)
+            {
+                python::object atp = f(idx);
+                rv = python::extract<std::string>(atp);
+                return rv;
+            }
+            return this->default_siteAtomType(idx);
+        }
+
+        const std::string& default_siteAtomType(int idx) const
+        {
+            return this->StructureAdapter::siteAtomType(idx);
+        }
+
+
         const R3::Vector& siteCartesianPosition(int idx) const
         {
             static R3::Vector rv;
@@ -71,6 +116,32 @@ class StructureAdapterWrap :
                 rv[i] = python::extract<double>(pos[i]);
             }
             return rv;
+        }
+
+
+        double siteMultiplicity(int idx) const
+        {
+            override f = this->get_override("siteMultiplicity");
+            if (f)  return f(idx);
+            return this->default_siteMultiplicity(idx);
+        }
+
+        double default_siteMultiplicity(int idx) const
+        {
+            return this->StructureAdapter::siteMultiplicity(idx);
+        }
+
+
+        double siteOccupancy(int idx) const
+        {
+            override f = this->get_override("siteOccupancy");
+            if (f)  return f(idx);
+            return this->default_siteOccupancy(idx);
+        }
+
+        double default_siteOccupancy(int idx) const
+        {
+            return this->StructureAdapter::siteOccupancy(idx);
         }
 
 
@@ -94,6 +165,20 @@ class StructureAdapterWrap :
             return rv;
         }
 
+
+        void customPQConfig(PairQuantity* pq) const
+        {
+            override f = this->get_override("_customPQConfig");
+            if (f)  f(pq);
+            else    this->default_customPQConfig(pq);
+        }
+
+        void default_customPQConfig(PairQuantity* pq) const
+        {
+            this->StructureAdapter::customPQConfig(pq);
+        }
+
+
 };  // class StructureAdapterWrap
 
 }   // namespace nswrap_StructureAdapter
@@ -106,20 +191,32 @@ void wrap_StructureAdapter()
 
     class_<StructureAdapterWrap, noncopyable>("StructureAdapter")
         .def("createBondGenerator",
-                &StructureAdapter::createBondGenerator,
-                return_value_policy<manage_new_object>())
-        .def("countSites", &StructureAdapter::countSites)
-        .def("totalOccupancy", &StructureAdapter::totalOccupancy)
-        .def("numberDensity", &StructureAdapter::numberDensity)
+                pure_virtual(&StructureAdapter::createBondGenerator),
+                return_internal_reference<>())
+        .def("countSites", pure_virtual(&StructureAdapter::countSites))
+        .def("totalOccupancy",
+                &StructureAdapter::totalOccupancy,
+                &StructureAdapterWrap::default_totalOccupancy)
+        .def("numberDensity", &StructureAdapter::numberDensity,
+                &StructureAdapterWrap::default_numberDensity)
         .def("siteAtomType", &StructureAdapter::siteAtomType,
+                &StructureAdapterWrap::default_siteAtomType,
                 return_value_policy<copy_const_reference>())
-        .def("siteCartesianPosition",
-                siteCartesianPosition_asarray<StructureAdapter,int>)
-        .def("siteMultiplicity", &StructureAdapter::siteMultiplicity)
-        .def("siteOccupancy", &StructureAdapter::siteOccupancy)
-        .def("siteAnisotropy", &StructureAdapter::siteAnisotropy)
+        .def("siteCartesianPosition", pure_virtual(
+                    siteCartesianPosition_asarray<StructureAdapter,int>))
+        .def("siteMultiplicity",
+                &StructureAdapter::siteMultiplicity,
+                &StructureAdapterWrap::default_siteMultiplicity)
+        .def("siteOccupancy",
+                &StructureAdapter::siteOccupancy,
+                &StructureAdapterWrap::default_siteOccupancy)
+        .def("siteAnisotropy",
+                pure_virtual(&StructureAdapter::siteAnisotropy))
         .def("siteCartesianUij",
-                siteCartesianUij_asarray<StructureAdapter,int>)
+                pure_virtual(siteCartesianUij_asarray<StructureAdapter,int>))
+        .def("_customPQConfig",
+                &StructureAdapter::customPQConfig,
+                &StructureAdapterWrap::default_customPQConfig)
         ;
 
     register_ptr_to_python<StructureAdapterPtr>();
