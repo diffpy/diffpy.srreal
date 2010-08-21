7dd6edb2d8c25690eb9ace3bd86ae1e477dce8bf
Pavol Juhas <pavol.juhas@gmail.com>
2010-08-21 00:41:57 +0000

    Changed handling of parallel calculations to use a custom factory for proxy calculator classes.

--- diffpy/srreal/parallel.py
+++ diffpy/srreal/parallel.py
@@ -21,81 +21,134 @@ into parallel calculators.
 __id__ = "$Id$"
 
 # exported items
-__all__ = ['ParallelPairQuantity']
+__all__ = ['createParallelCalculator']
+
+import copy
+import inspect
 
 # ----------------------------------------------------------------------------
 
-class ParallelPairQuantity(object):
+def createParallelCalculator(pqobj, ncpu, pmap):
+    '''Create a proxy parallel calculato to a normal PairQuantity instance.
 
-    '''Class for running parallel calculations.  This is a proxy class
-    with the same interface as the wrapped PairQuantity type.
-
-    Instance data:
-
-    pqclass  -- some concrete PairQuantity class.
+    pqobj    -- instance of PairQuantity calculator to be run in parallel
     ncpu     -- number of parallel jobs
     pmap     -- a parallel map function used to submit job to workers
+
+    Return a proxy calculator instance that has the same interface,
+    but runs calculation in parallel, split into ncpu jobs.
     '''
 
-    def __init__(self, pqtype, ncpu, pmap, **kwargs):
-        '''Initialize a parallel proxy to the PairQuantity pqtype
+    class ParallelPairQuantity(object):
+
+        '''Class for running parallel calculations.  This is a proxy class
+        to the wrapper PairQuantity type with the same interface.
+
+        Instance data:
 
         pqclass  -- some concrete PairQuantity class.
         ncpu     -- number of parallel jobs
         pmap     -- a parallel map function used to submit job to workers
-        kwargs   -- optional keyword arguments that are passed to the
-                    pqclass constructor
-
-        No return value.
         '''
-        self.pqtype = pqtype
-        self.ncpu = ncpu
-        self.pmap = pmap
-        self._pqobj = self.pqtype(**kwargs)
-        return
+
+        def __init__(self, pqobj, ncpu, pmap):
+            '''Initialize a parallel proxy to the PairQuantity instance.
+
+            pqobj    -- instance of PairQuantity calculator to be run in parallel
+            ncpu     -- number of parallel jobs
+            pmap     -- a parallel map function used to submit job to workers
+
+            No return value.
+            '''
+            self.pqobj = pqobj
+            self.ncpu = ncpu
+            self.pmap = pmap
+            return
 
 
-    def eval(self, structure):
-        '''Perform parallel calculation and return internal value array.
+        def eval(self, structure):
+            '''Perform parallel calculation and return internal value array.
 
-        structure    -- structure to be evaluated, an instance of
-                        diffpy Structure or pyobjcryst Crystal
-        kwargs       -- optional parameter settings for this calculator
+            structure    -- structure to be evaluated, an instance of
+                            diffpy Structure or pyobjcryst Crystal
 
-        Return numpy array.
-        '''
-        self._pqobj.setStructure(structure)
-        kwd = { 'ncpu' : self.ncpu,
-                'pqobj' : self._pqobj,
-                'structure' : structure }
-        arglist = [kwd.copy() for kwd['cpuindex'] in range(self.ncpu)]
-        for y in self.pmap(_partialValue, arglist):
-            self._pqobj._mergeParallelValue(y)
-        return self._pqobj.value()
+            Return numpy array.
+            '''
+            self.pqobj.setStructure(structure)
+            kwd = { 'ncpu' : self.ncpu,
+                    'pqobj' : copy.copy(self.pqobj),
+                    'structure' : structure }
+            arglist = [kwd.copy() for kwd['cpuindex'] in range(self.ncpu)]
+            for y in self.pmap(_partialValue, arglist):
+                self.pqobj._mergeParallelValue(y)
+            return self.pqobj.value()
 
 
-    def __call__(self, *args, **kwargs):
-        '''Call the wrapped instance as a function, but with parallel evaluation.
-        Uses the same arguments and return values as wrapped pqtype.
-        '''
-        savedeval = self._pqobj.__dict__.get('eval')
-        def restore_eval():
-            if savedeval:
-                self._pqobj.eval = savedeval
-            else:
-                self._pqobj.__dict__.pop('eval', None)
-        def parallel_eval(structure):
-            assert self._pqobj.eval is parallel_eval
-            restore_eval()
-            return self.eval(structure)
-        self._pqobj.eval = parallel_eval
-        try:
-            rv = self._pqobj(*args, **kwargs)
-        finally:
-            restore_eval()
-        return rv
+        def __call__(self, *args, **kwargs):
+            '''Call the wrapped instance as a function, but with parallel evaluation.
+            Uses the same arguments and return values as the wrapped PairQuantity.
+            '''
+            savedeval = self.pqobj.__dict__.get('eval')
+            def restore_eval():
+                if savedeval:
+                    self.pqobj.eval = savedeval
+                else:
+                    self.pqobj.__dict__.pop('eval', None)
+            def parallel_eval(structure):
+                assert self.pqobj.eval is parallel_eval
+                restore_eval()
+                return self.eval(structure)
+            self.pqobj.eval = parallel_eval
+            try:
+                rv = self.pqobj(*args, **kwargs)
+            finally:
+                restore_eval()
+            return rv
 
-# class ParallelPairQuantity
+    # class ParallelPairQuantity
+
+    # Create proxy method and properties to the wrapped PairQuantity
+
+    pqtype = type(pqobj)
+
+    # create proxy methods to all public methods and some protected methods
+
+    proxy_protected = set('''_getDoubleAttr _setDoubleAttr _hasDoubleAttr
+        _namesOfDoubleAttributes _namesOfWritableDoubleAttributes'''.split())
+
+    def _make_proxymethod(name):
+        f = getattr(pqtype, name)
+        def proxymethod(self, *args, **kwargs):
+            return f(self.pqobj, *args, **kwargs)
+        proxymethod.__name__ = name
+        proxymethod.__doc__ = f.__doc__
+        return proxymethod
+
+    for n, f in inspect.getmembers(pqtype, inspect.ismethod):
+        ignore = (hasattr(ParallelPairQuantity, n) or
+                (n.startswith('_') and n not in proxy_protected))
+        if ignore:  continue
+        setattr(ParallelPairQuantity, n, _make_proxymethod(n))
+
+    # create proxy properties to all properties that do not conflict with
+    # existing class items
+
+    def _make_proxyproperty(prop):
+        fget = fset = fdel = None
+        if prop.fget:
+            def fget(self):  return prop.fget(self.pqobj)
+        if prop.fset:
+            def fset(self, value):  return prop.fset(self.pqobj, value)
+        if prop.fdel:
+            def fdel(self):  return prop.fdel(self.pqobj)
+        return property(fget, fset, fdel, prop.__doc__)
+
+    for n, p in inspect.getmembers(pqtype, lambda x: type(x) is property):
+        if hasattr(ParallelPairQuantity, n):  continue
+        setattr(ParallelPairQuantity, n, _make_proxyproperty(p))
+
+    # finally create an instance of this very custom class
+    return ParallelPairQuantity(pqobj, ncpu, pmap)
 
 
 def _partialValue(kwd):
