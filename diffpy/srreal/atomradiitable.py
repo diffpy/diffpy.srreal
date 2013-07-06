a79caa85bb123d58853f73b93ce47b0b7d5abe67
Pavol Juhas <pavol.juhas@gmail.com>
2013-07-06 13:07:04 -0400

    Register CovalentRadiiTable on module import.
    Also delay the import of periodictable until the first call
    of CovalentRadiiTable.lookup.  This allows to register this class
    even if periodictable is not installed.

--- diffpy/srreal/atomradiitable.py
+++ diffpy/srreal/atomradiitable.py
@@ -34,24 +34,6 @@ class CovalentRadiiTable(AtomRadiiTable):
 
     # class variable that holds the periodictable.elements object
     _elements = None
-    # flag that checks if this class has been already registered
-    __registered = False
-
-    def __init__(self):
-        '''Initialize the CovalentRadiiTable class.
-        This makes sure that the periodictable.elements can be imported.
-        '''
-        super(CovalentRadiiTable, self).__init__()
-        if self._elements is None:
-            from periodictable import elements
-            CovalentRadiiTable._elements = elements
-        assert self._elements is not None
-        if not CovalentRadiiTable.__registered:
-            CovalentRadiiTable.__registered = True
-            self._registerThisType()
-            assert self.type() in AtomRadiiTable.getRegisteredTypes()
-        return
-
 
     def _tableLookup(self, smbl):
         '''Return covalent atom radius in Angstroms.
@@ -60,6 +42,9 @@ class CovalentRadiiTable(AtomRadiiTable):
 
         Return float.  Raise ValueError for unknown element symbol.
         '''
+        if CovalentRadiiTable._elements is None:
+            from periodictable import elements
+            CovalentRadiiTable._elements = elements
         e = self._elements.isotope(smbl)
         rv = e.covalent_radius
         if rv is None:
@@ -92,6 +77,6 @@ class CovalentRadiiTable(AtomRadiiTable):
         return "covalentradii"
 
 # End of class CovalentRadiiTable
-
+CovalentRadiiTable()._registerThisType()
 
 # End of file
