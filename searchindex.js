e6410d2a7aa63d7d43ec4a12fb805b8127c6b3fe
Pavol Juhas <pavol.juhas@gmail.com>
2013-10-31 16:04:00 -0400

    Synced with the source at c05c828.

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,modul:0,indic:0,content:0,srreal:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to SrReal&#8217;s documentation!"],objnames:{},filenames:["index"]})
+Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,modul:0,indic:0,content:0,srreal:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to SrReal documentation!"],objnames:{},filenames:["index"]})
