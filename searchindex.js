a1927ff432995611005959207d3d0e6a8dc577d2
Pavol Juhas <pavol.juhas@gmail.com>
2014-01-29 11:00:12 -0500

    Synced with the source at 7769f1fffcb5881ec45c8b4c6d05a2a688bf8efc.

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,modul:0,indic:0,content:0,srreal:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to SrReal documentation!"],objnames:{},filenames:["index"]})
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to SrReal documentation!"],objects:{},titleterms:{srreal:0,document:0,welcom:0,indic:0,tabl:0}})
