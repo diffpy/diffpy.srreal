223e135542d1191d8cf90e7437523922498f9274
Pavol Juhas <pavol.juhas@gmail.com>
2016-04-06 16:24:18 -0500

    Use IPython parallel imap function.
    Direct node execution is slow.

--- timing/parallelbench02.py
+++ timing/parallelbench02.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 import multiprocessing
 import numpy as np
 import time
-import sys
 from diffpy.Structure import Structure
 from diffpy.srreal.pdfcalculator import PDFCalculator
 from diffpy.srreal.structureadapter import createStructureAdapter
@@ -48,29 +47,18 @@ class ParallelCalculator(object):
     def __init__(self, ncpu):
         self.pcmaster = pdfcstd.copy()
         self.ncpu = ncpu
-        dv.push(dict(pc=self.pcmaster.copy()))
-        for i, vv in enumerate(rc):
-            vv.execute('pc._setupParallelRun(%i, %i)' %
-                    (i, self.ncpu))
         return
 
 
     def __call__(self, stru):
         self.pcmaster.setStructure(stru)
-        dv.push(dict(stru=stru))
-        dv.execute('pc.eval(stru); pdata = pc._getParallelData()')
-        for i, data in enumerate(dv.pull('pdata')):
-            if i == self.ncpu:  break
+        arglist = [(self.pcmaster, i, self.ncpu)
+                for i in range(self.ncpu)]
+        for data in dv.imap(partialresult, arglist):
             self.pcmaster._mergeParallelData(data, self.ncpu)
         rv = (self.pcmaster.rgrid, self.pcmaster.pdf)
         return rv
 
-
-r0, g0 = pc0(adpt)
-ppc = ParallelCalculator(4)
-r1, g1 = ppc(adpt)
-
-print("parallel calculator consistent:", np.allclose(g0, g1))
 
 print("time on a single thread:", timecalculator(pc0))
 
