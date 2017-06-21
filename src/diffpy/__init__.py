906eadd6892e693886e1131cdcd0e9326e2d0d52
Pavol Juhas <pavol.juhas@gmail.com>
2017-06-21 18:24:12 -0400

    PY3: declare namespace package with pkgutil module.
    Use the builtin module instead of pkg_resources from setuptools.
    This is compatible with PEP 420 namespace packages in Python 3.

--- src/diffpy/__init__.py
+++ src/diffpy/__init__.py
@@ -20,7 +20,8 @@ Blank namespace package.
 """
 
 
-__import__('pkg_resources').declare_namespace(__name__)
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
 
 
 # End of file
