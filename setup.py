2bfbb5710e411b2170f829a5d1d6c7b95b00fae8
Pavol Juhas <pavol.juhas@gmail.com>
2018-01-11 17:27:55 -0500

    REL: exclude gitarchive.cfg from package files
    - move gitarchive.cfg to repository root
    - exclude it from source distribution tarball
    - exclude it from installation with package data
    - fix test for expanded .gitarchive.cfg in setup.py

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ def create_extensions():
 # It must reside in the same directory as version.py.
 MYDIR = os.path.dirname(os.path.abspath(__file__))
 versioncfgfile = os.path.join(MYDIR, 'src/diffpy/srreal/version.cfg')
-gitarchivecfgfile = versioncfgfile.replace('version.cfg', 'gitarchive.cfg')
+gitarchivecfgfile = os.path.join(MYDIR, '.gitarchive.cfg')
 
 
 def gitinfo():
@@ -105,7 +105,7 @@ def getversioncfg():
     g = vd0.copy()
     cp0 = RawConfigParser(vd0)
     cp0.read(gitarchivecfgfile)
-    if '$Format:' not in cp0.get('DEFAULT', 'commit'):
+    if len(cp0.get('DEFAULT', 'commit')) > 20:
         g = cp0.defaults()
         mx = re.search(r'\btag: v(\d[^,]*)', g.pop('refnames'))
         if mx:
