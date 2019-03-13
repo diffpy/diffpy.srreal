5e0be28aef3f86e7062d1cbe9e39c28558255c9b
Pavol Juhas <pavol.juhas@gmail.com>
2019-03-13 15:47:16 -0400

    DOC: activate m2r for sphinx/markdown integration

--- doc/manual/source/conf.py
+++ doc/manual/source/conf.py
@@ -37,13 +37,16 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
+    'm2r',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# The suffix of source filenames.
-source_suffix = '.rst'
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 # source_encoding = 'utf-8-sig'
