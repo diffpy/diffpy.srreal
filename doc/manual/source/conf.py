bb3f48b70090f7fcb02a27e8bf8045cf45f1d94d
Pavol Juhas <pavol.juhas@gmail.com>
2019-03-13 15:20:29 -0400

    DOC: drop unused extension `doctest`

--- doc/manual/source/conf.py
+++ doc/manual/source/conf.py
@@ -31,8 +31,11 @@ ab_authors = 'Pavol Juhás, Christopher L. Farrow, Simon J.L. Billinge group'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
-              'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
