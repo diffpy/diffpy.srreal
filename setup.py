5cbb9ba531234c6cf44cbcf775a4b702f68cabda
Sparks29032 <Sparks29032@gmail.com>
2024-05-25 19:47:34 -0400

    Initial pyproject.toml

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import os
 import re
 import sys
 import glob
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools import Extension
 from numpy.distutils.misc_util import get_numpy_include_dirs
 
@@ -78,115 +78,11 @@ def create_extensions():
     return [ext]
 
 
-# versioncfgfile holds version data for git commit hash and date.
-# It must reside in the same directory as version.py.
-MYDIR = os.path.dirname(os.path.abspath(__file__))
-versioncfgfile = os.path.join(MYDIR, 'src/diffpy/srreal/version.cfg')
-gitarchivecfgfile = os.path.join(MYDIR, '.gitarchive.cfg')
-
-
-def gitinfo():
-    from subprocess import Popen, PIPE
-    kw = dict(stdout=PIPE, cwd=MYDIR, universal_newlines=True)
-    proc = Popen(['git', 'describe', '--match=v[[:digit:]]*'], **kw)
-    desc = proc.stdout.read()
-    proc = Popen(['git', 'log', '-1', '--format=%H %ct %ci'], **kw)
-    glog = proc.stdout.read()
-    rv = {}
-    rv['version'] = '.post'.join(desc.strip().split('-')[:2]).lstrip('v')
-    rv['commit'], rv['timestamp'], rv['date'] = glog.strip().split(None, 2)
-    return rv
-
-
-def getversioncfg():
-    if PY3:
-        from configparser import RawConfigParser
-    else:
-        from ConfigParser import RawConfigParser
-    vd0 = dict(version=FALLBACK_VERSION, commit='', date='', timestamp=0)
-    # first fetch data from gitarchivecfgfile, ignore if it is unexpanded
-    g = vd0.copy()
-    cp0 = RawConfigParser(vd0)
-    cp0.read(gitarchivecfgfile)
-    if len(cp0.get('DEFAULT', 'commit')) > 20:
-        g = cp0.defaults()
-        mx = re.search(r'\btag: v(\d[^,]*)', g.pop('refnames'))
-        if mx:
-            g['version'] = mx.group(1)
-    # then try to obtain version data from git.
-    gitdir = os.path.join(MYDIR, '.git')
-    if os.path.exists(gitdir) or 'GIT_DIR' in os.environ:
-        try:
-            g = gitinfo()
-        except OSError:
-            pass
-    # finally, check and update the active version file
-    cp = RawConfigParser()
-    cp.read(versioncfgfile)
-    d = cp.defaults()
-    rewrite = not d or (g['commit'] and (
-        g['version'] != d.get('version') or g['commit'] != d.get('commit')))
-    if rewrite:
-        cp.set('DEFAULT', 'version', g['version'])
-        cp.set('DEFAULT', 'commit', g['commit'])
-        cp.set('DEFAULT', 'date', g['date'])
-        cp.set('DEFAULT', 'timestamp', g['timestamp'])
-        with open(versioncfgfile, 'w') as fp:
-            cp.write(fp)
-    return cp
-
-versiondata = getversioncfg()
-
-with open(os.path.join(MYDIR, 'README.rst')) as fp:
-    long_description = fp.read()
-
-# define distribution
+# Extensions not included in pyproject.toml
 setup_args = dict(
-    name = "diffpy.srreal",
-    version = versiondata.get('DEFAULT', 'version'),
-    packages = find_packages(os.path.join(MYDIR, 'src')),
-    package_dir = {'' : 'src'},
-    test_suite = 'diffpy.srreal.tests',
-    include_package_data = True,
     ext_modules = [],
-    install_requires = [
-        'diffpy.structure',
-    ],
-    zip_safe = False,
-
-    author = "Simon J.L. Billinge group",
-    author_email = "sb2896@columbia.edu",
-    maintainer = "Pavol Juhas",
-    maintainer_email = "pavol.juhas@gmail.com",
-    description = ("calculators for PDF, bond valence sum, and other "
-                   "quantities based on atom pair interaction."),
-    long_description = long_description,
-    long_description_content_type = 'text/x-rst',
-    license = 'BSD-style license',
-    url = "https://github.com/diffpy/diffpy.srreal/",
-    keywords = "PDF BVS atom overlap calculator real-space",
-    classifiers = [
-        # List of possible values at
-        # http://pypi.python.org/pypi?:action=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
-        'Programming Language :: C++',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Scientific/Engineering :: Chemistry',
-        'Topic :: Scientific/Engineering :: Physics',
-        'Topic :: Software Development :: Libraries',
-    ],
 )
+
 
 if __name__ == '__main__':
     setup_args['ext_modules'] = create_extensions()
